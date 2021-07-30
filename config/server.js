3ad00e165cf20f42d9248a638c6fea995f9e9656
AlexPG96 <ap96.gonzalez@gmail.com>
2021-07-30 20:18:42 +0200

    app ecommerce strapi with vue3

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ca6fc47ef306620dccbf0b288cbd5283'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '1ae64ad2-c3a2-45cc-ae87-a2d006a15e56'
+};
